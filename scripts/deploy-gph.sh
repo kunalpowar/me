a712e5b38ed28aa6fd7168191365ab9d5c326a4a
Kunal U Powar <kunalpowar1203@gmail.com>
2018-11-07 12:11:06 +0530

    repo changes

--- scripts/deploy-gph.sh
+++ scripts/deploy-gph.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-ng build --prod --base-href "https://kunalpowar.github.io/kunalpowar.github.io"
-npx ngh --dir=dist/kunalpowar-githubpages
+ng build --prod --base-href "https://kunalpowar.github.io/me/"
+npx ngh --dir=dist/me
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'kunalpowar-githubpages'`, () => {
+  it(`should have as title 'me'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('kunalpowar-githubpages');
+    expect(app.title).toEqual('me');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to kunalpowar-githubpages!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to me!');
   });
 });
